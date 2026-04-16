7b51e7237e4efe64bade1cd8387b0cb18874fbd6
Nair Paez <nairpaez@outlook.com>
2026-04-16 19:23:49 -0300

    guardamos las tareas en el arrayTareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import ListaDeTarea from "./ListaDeTarea";
 
 const FormularioTarea = () => {
+  const [arrayTareas, setArrayaTareas] = useState([]);
+  const [tarea, setTarea] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setArrayaTareas([...arrayTareas, tarea]);
+  };
+
   return (
     <section>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3 d-flex ">
           <input
             type="text"
             className="form-control"
             id="inputTarea"
             placeholder="Ingresa una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
           />
           <button className="btn btn-primary">Enviar</button>
         </div>
--- src/components/ListaDeTarea.jsx
+++ src/components/ListaDeTarea.jsx
@@ -3,9 +3,6 @@ import ItemTarea from "./ItemTarea";
 const ListaDeTarea = () => {
   return <ul className="list-group">
     <ItemTarea></ItemTarea>
-    <ItemTarea></ItemTarea>
-    <ItemTarea></ItemTarea>
-    <ItemTarea></ItemTarea>
   </ul>;
 };
 
